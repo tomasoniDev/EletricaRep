797e37242a86e21b5cfbe3eab70520e1baaa8363
EletricalRep <desenvolvimento@tomasoni.ind.br>
2026-08-06 11:06:04 -0300

    Reforcar criterios PWA Android

--- public/sw.js
+++ public/sw.js
@@ -6,6 +6,6 @@ self.addEventListener("activate", (event) => {
   event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener("fetch", () => {
-  return;
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request));
 });
